5f23d9ce479635dc25a160a0ba0264e235bdf300
roquealex <roquealex20@hotmail.com>
2019-01-03 12:08:46 -0800

    Non function mode

--- python/wundCrawler/wundCrawler.py
+++ python/wundCrawler/wundCrawler.py
@@ -16,19 +16,43 @@ print("Crawler test")
 #specify the file as a pandas.Timestamp 
 
 path = '.'
-hier = False
-#hier = True
+#hier = False
+hier = True
 hier_patt = '%Y/%m/'
 format = 'Csv'
 pwsID = 'IYUCATNT2'
 ts = pd.Timestamp(year=2017, month=1, day=1)
 skip=True
 
+# This string version of the date will be useful in the whole function
+dateStr = ts.strftime('%Y-%m-%d')
+
+# First calculate filenames to see if the file exist and we need to skip
+fileBaseName = pwsID+'-'+dateStr
+
+hierPath = ''
+if hier :
+    hierPath = ts.strftime(hier_patt)
+
+basePath = path+'/'+hierPath
+
+fileBasePath = basePath+fileBaseName
+
+fileName = fileBasePath+('.csv' if format.lower() == 'csv' else '.txt')
+if skip and os.path.exists(fileName) :
+    print(fileName,"Already exists skiping it...")
+    # TODO change for return when this is a function
+    exit(0)
+
+'''
+if format.lower() == 'csv' :
+    fileName = fileBasePath+'.csv'
+else :
+    fileName = fileBasePath+'.txt'
+'''
 
 parameters = {'ID':pwsID,'month':ts.month,'day':ts.day,'year':ts.year,'format':1}
 
-#dateStr = ts.strftime('yyyy-MM-dd')
-dateStr = ts.strftime('%Y-%m-%d')
 
 # Other tries:
 #parameters = {'ID':'IYUCATNT2','month':1,'day':1,'year':2017}
@@ -52,12 +76,12 @@ resp.raise_for_status()
 # Binary:
 #print(resp.content)
 # text:
-print(resp.text)
+#print(resp.text)
 #?ID=IYUCATNT2&month=1&day=1&year=2017&format=5')
 
 #print(resp.content.decode('utf-8').split('\n')[1])
 lines = resp.text.split('\n')
-print(lines[1])
+#print(lines[1])
 
 #    '^Time,TemperatureF,.*,WindDirectionDegrees,WindSpeedMPH,.*0,DateUTC\S$',
 #    '^Time,TemperatureF,.*,WindDirectionDegrees,WindSpeedMPH,.*0,DateUTC\S*$',
@@ -67,24 +91,13 @@ valid = re.match(
 
 assert valid, 'Invalid format for date '+dateStr+', header not found'
 
-print(dateStr)
 
-fileBaseName = pwsID+'-'+dateStr
-
-hierPath = ''
-if hier :
-    hierPath = ts.strftime(hier_patt)
-    
-
-basePath = path+'/'+hierPath
 setupPath(basePath)
 
-fileBasePath = basePath+fileBaseName
 
-format = ''
 if format.lower() == 'csv' :
-    print('Saving CSV file')
-    fileName = fileBasePath+'.csv'
+    print('Writing CSV file',fileName)
+    #fileName = fileBasePath+'.csv'
     #noBrLines lines.map(lambda x: re.sub(r'<br>|,$',r'',x),lines)
     # The response from wunderground is almost a csv but it has <br> and a coma at the end
     noBrLines = map(lambda x: re.sub(r'<br>|,$',r'',x),lines)
@@ -94,8 +107,8 @@ if format.lower() == 'csv' :
     f.write('\n'.join(noBlankLines))
     f.close()
 else :
-    fileName = fileBasePath+'.txt'
-    print('Saving raw file',)
+    #fileName = fileBasePath+'.txt'
+    print('Writing raw file',fileName)
     # Raw file from the binary response
     f= open(fileName,"wb")
     f.write(resp.content)
