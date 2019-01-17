ee71cbcd6a4f95b92532f018d89163fa2fbab0c0
roquealex <roquealex20@hotmail.com>
2019-01-16 21:04:58 -0800

    index is a copy of number

--- perl/index/index.pl
+++ perl/index/index.pl
@@ -1,9 +1,63 @@
 use strict;
 use warnings;
 
-my $filename = "kbWindNorYuc.py";
+my $filename = $ARGV[0];
 open(my $fh, "$filename") or die "Could not open $filename $!";
 
+my @sub_arr;
+my @table_arr;
+
+my @file = <$fh>;
+#while (my $line = @file) {
+foreach my $line (@file) {
+  #print $line;
+  if ($line =~ m/\s*"\s*#(#+)\s(.*?)\s*(\\n)?"/) {
+    my $len = length($1);
+    my $title = $2;
+    if (scalar(@sub_arr) > $len) {
+      while(scalar(@sub_arr)!=$len){
+        pop(@sub_arr);
+      }
+      @sub_arr[-1]++;
+    } elsif (scalar(@sub_arr) < $len) {
+      while(scalar(@sub_arr)!=$len){
+        push(@sub_arr,1);
+      }
+    } else {
+      @sub_arr[-1]++;
+    }
+    my $sub = join('.',@sub_arr);
+    #print("$sub $title\n");
+    $title = "$sub $title";
+    my $linkname = $title;
+    $linkname =~ s/\s/-/g;
+    my $entry = ' ' x (($len-1)*2)  . "* [$title](#$linkname)\n";
+    push(@table_arr,$entry);
+    $line =~ s/(#+)/$1 $sub/;
+    #print $line;
+    #print $len."\n"
+  }
+}
+close($fh);
+
+open(my $ofh, ">$filename") or die "Could not open $filename $!";
+foreach my $line (@file) {
+  print $ofh $line;
+}
+close($ofh);
+
+foreach my $e (@table_arr) {
+  print $e;
+}
+
+#my @sub_arr = (1,2,3);
+#push(@sub_arr,4);
+#print "@sub_arr\n";
+#print "$sub_arr[-1]\n";
+#my $dummy = <<'END';
+
+
+my $dummy = << 'END'
 while (my $line = <$fh>) {
   chomp $line;
   if ($line =~ m/^# (#+)\s*(.*?)\s*$/) {
@@ -17,8 +71,7 @@ while (my $line = <$fh>) {
     print ' ' x (($len-$level)*2)  . "* [$title](#$linkname)\n";
   }
 }
-close($fh);
-
+END
 
 
 
