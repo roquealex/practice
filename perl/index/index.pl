fa301709a05729f916749d0bc57cde42e52856a3
roquealex <roquealex20@hotmail.com>
2019-01-16 19:15:44 -0800

    script works on python script

--- perl/index/index.pl
+++ perl/index/index.pl
@@ -1,8 +1,23 @@
 use strict;
-use warning;
+use warnings;
 
-my $filename = 
-open(my $fh, "$filename")
+my $filename = "kbWindNorYuc.py";
+open(my $fh, "$filename") or die "Could not open $filename $!";
+
+while (my $line = <$fh>) {
+  chomp $line;
+  if ($line =~ m/^# (#+)\s*(.*?)\s*$/) {
+    #print $line.": '$1' '$2'\n";
+    my $level = 2;
+    my $len = length($1);
+    my $title = $2;
+    my $linkname = $title;
+    $linkname =~ s/\s/-/g;
+    next if ($len < $level);
+    print ' ' x (($len-$level)*2)  . "* [$title](#$linkname)\n";
+  }
+}
+close($fh);
 
 
 
