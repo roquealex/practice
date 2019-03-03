762db627302e2e72a73655804b386cb2ca80a71c
Ubuntu <ubuntu@ip-172-31-17-59.us-west-2.compute.internal>
2019-03-03 23:09:05 +0000

    adding more python

--- aws/ec2/ubuntu_init.sh
+++ aws/ec2/ubuntu_init.sh
@@ -13,3 +13,15 @@ sudo apt-get -y install xfce4 gnome-session
 sudo apt-get -y install xfce4 gnome-panel
 sudo apt-get -y install xfce4 gnome-settings-daemon
 
+# gvim:
+sudo apt-get -y install vim-gtk3
+
+# Python (needed for crawler):
+sudo apt-get -y install python3-pip
+
+# Python packages
+pip3 install pandas
+
+
+
+
